2b5038f2583eb279985d31eef4dea8719bc4d728
shubhamg931 <sg270798@gmail.com>
2019-02-14 16:57:07 +0530

    corrected merge sort

--- IN_PLACE_MERGE_SORT.cpp
+++ IN_PLACE_MERGE_SORT.cpp
@@ -1,71 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
-swap function to swap two elements in the
-array
-*/
-void swap(int &a,int &b)
+void merge(int arr[], int start, int mid, int end)
 {
-    int t = a;
-    a = b;
-    b = t;
-}
+    int start2 = mid + 1;
 
-
-//merge function to merge two sorted arrays
-void merge(int arr[],int s,int e)
-{
-    int mid = (s+e)/2;
-    int i,j;
-    i=s;j=mid+1;
-    while(i<=mid && j<=e)
-    {
-        if(arr[i]<=arr[j])
-        {
-            i++;
-        }
-        else
-        {
-            swap(arr[i],arr[j]);
-            j++;
-        }
-    }
-}
-
-/*
-mergeSort function recursively call its left half and
-right half and the call merge function to merge the two
-sorted halves
-*/
-void mergeSort(int arr[],int s,int e)
-{
-    if(s==e)
+    // If the direct merge is already sorted
+    if (arr[mid] <= arr[start2]) {
         return;
-    int mid = (s+e)/2;
-    mergeSort(arr,s,mid);
-    mergeSort(arr,mid+1,e);
-    merge(arr,s,e);
-}
-
-int main() {
-    int n, a[100001], i;
-    //size of the input array
-    cin >> n;
-
-    //input array
-    for (i = 0; i < n; i++) {
-        cin >> a[i];
     }
 
-    /*
-    calling mergeSort on input array
-    mergeSort(array,start,end)
-    */
-    mergeSort(a, 0, n - 1);
+    // Two pointers to maintain start
+    // of both arrays to merge
+    while (start <= mid && start2 <= end) {
 
-    //sorted array
-    for (i = 0; i < n; i++) cout << a[i] << " ";
+        // If element 1 is in right place
+        if (arr[start] <= arr[start2]) {
+            start++;
+        }
+        else {
+            int value = arr[start2];
+            int index = start2;
 
+            // Shift all the elements between element 1
+            // element 2, right by 1.
+            while (index != start) {
+                arr[index] = arr[index - 1];
+                index--;
+            }
+            arr[start] = value;
+
+            // Update all the pointers
+            start++;
+            mid++;
+            start2++;
+        }
+    }
+}
+
+/* l is for left index and r is right index of the
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+
+        // Same as (l + r) / 2, but avoids overflow
+        // for large l and r
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+/* UTILITY FUNCTIONS */
+/* Function to print an array */
+void printArray(int A[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout<<A[i]<<" ";
+    cout<<"\n";
+}
+
+/* Driver program to test above functions */
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    printArray(arr, arr_size);
     return 0;
 }
